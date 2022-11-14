2d955e64e62652d8cd15b94e34ec5e19bf4aba34
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-11-14 18:31:43 -0500

    refactor(*.java)

--- app/src/main/java/com/example/b07/MainActivity.java
+++ app/src/main/java/com/example/b07/MainActivity.java
@@ -36,13 +36,11 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        binding.fab.setOnClickListener(
+            view -> Snackbar.make(
+                view, "Replace with your own action", Snackbar.LENGTH_LONG
+            ).setAction("Action", null).show()
+        );
     }
 
     @Override
@@ -70,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 }
