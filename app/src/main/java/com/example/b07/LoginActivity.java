c37fe39104186ee5187d79fd585a620275f48344
Jiaming Lu <jiaming.lu.0124@gamil.com>
2022-11-15 23:29:24 -0500

    created loging activity

---
+++ app/src/main/java/com/example/b07/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.example.b07;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText email, password;
+    private Button btnLogin;
+    private TextView textRegister;
+
+    protected void onCreated(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.login_btn);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+    }
+
+    private void login() {
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+
+        if (user.isEmpty()) {
+            email.setError("Email cannot be empty");
+        }
+        if (pass.isEmpty()) {
+            password.setError("Password cannot be empty");
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "Login Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+}
