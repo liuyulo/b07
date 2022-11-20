c22e0573eb286db9434f83b7195d2f5d0baa3214
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-11-20 16:26:23 -0500

    refactor(Timeline) move classes into files

---
+++ app/src/main/java/com/example/b07/Semester.java
@@ -0,0 +1,24 @@
+package com.example.b07;
+
+class Semester {
+    int year;
+    Session session;
+
+    public Semester(int year, Session session) {
+        this.year = year;
+        this.session = session;
+    }
+
+    public Semester next() {
+        return switch (session) {
+            case WINTER -> new Semester(year, Session.SUMMER);
+            case SUMMER -> new Semester(year, Session.FALL);
+            case FALL -> new Semester(year + 1, Session.WINTER);
+        };
+    }
+
+    @Override
+    public String toString() {
+        return year + " " + session;
+    }
+}
