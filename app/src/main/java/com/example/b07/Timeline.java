a16a31d04d0ce3b9c0acd6cd6357d3eebffecdba
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-11-21 08:58:48 -0500

    refactor(Timeline) use `Course`

--- app/src/main/java/com/example/b07/Timeline.java
+++ app/src/main/java/com/example/b07/Timeline.java
@@ -3,23 +3,6 @@ package com.example.b07;
 import java.util.*;
 import java.util.stream.Collectors;
 
-class Cours {
-    String code;
-    Set<Session> sessions;
-    Set<Cours> prereqs;
-
-    public Cours(String code, Set<Session> sessions, Set<Cours> prereqs){
-        this.code = code;
-        this.sessions = sessions;
-        this.prereqs = prereqs;
-    }
-
-    @Override
-    public String toString(){
-        return code;
-    }
-}
-
 public class Timeline{
     /**
      * Generate timeline
@@ -29,13 +12,13 @@ public class Timeline{
      * @param current the semester to start with
      * @return generated timeline
      */
-    public static Map<String, Set<Cours>> generate(Set<Cours> t, Set<Cours> w, Semester current){
+    public static Map<String, Set<Course>> generate(Set<Course> t, Set<Course> w, Semester current){
         // don't want to mutate the input
-        Set<Cours> taken = new HashSet<>(t);
-        Set<Cours> want = new HashSet<>(w);
+        Set<Course> taken = new HashSet<>(t);
+        Set<Course> want = new HashSet<>(w);
 
         // This set will record necessary courses to form the timeline which missing in Set w.
-        Set<Cours> need;
+        Set<Course> need;
         do{
             need = want.stream().flatMap(course -> course.prereqs.stream().filter(
                 prereq -> !(taken.contains(prereq) || want.contains(prereq))
@@ -44,13 +27,13 @@ public class Timeline{
         }while(!need.isEmpty());
         // After the loop, we added all necessary courses into want.
 
-        Map<String, Set<Cours>> output = new LinkedHashMap<>();
+        Map<String, Set<Course>> output = new LinkedHashMap<>();
 
         // This loop go through courses in want and add those which can be taken in current semester
         // Then go to next semester and repeat. Until want is empty.
         while(!want.isEmpty()){
-            Set<Cours> toTake = new HashSet<>();
-            for(Cours course : want){
+            Set<Course> toTake = new HashSet<>();
+            for(Course course : want){
                 if(taken.containsAll(course.prereqs) && course.sessions.contains(current.session)){
                     toTake.add(course);
                 }
@@ -64,23 +47,23 @@ public class Timeline{
     }
 
     public static void main(String[] args){
-        Cours a08 = new Cours("CSCA08", Session.FW, new HashSet<>());
-        Cours a67 = new Cours("CSCA67", Session.FW, new HashSet<>());
+        Course a08 = new Course("CSCA08", Session.FW, new HashSet<>());
+        Course a67 = new Course("CSCA67", Session.FW, new HashSet<>());
 
-        Cours a48 = new Cours("CSCA48", Session.WS, new HashSet<>(List.of(a08)));
-        Cours b07 = new Cours("CSCB07", Session.FS, new HashSet<>(List.of(a48)));
-        Cours b09 = new Cours("CSCB09", Session.WS, new HashSet<>(List.of(a48)));
+        Course a48 = new Course("CSCA48", Session.WS, new HashSet<>(List.of(a08)));
+        Course b07 = new Course("CSCB07", Session.FS, new HashSet<>(List.of(a48)));
+        Course b09 = new Course("CSCB09", Session.WS, new HashSet<>(List.of(a48)));
 
-        Cours b36 = new Cours("CSCB36", Session.FS, new HashSet<>(List.of(a48, a67)));
-        Cours b63 = new Cours("CSCB63", Session.WS, new HashSet<>(List.of(b36)));
+        Course b36 = new Course("CSCB36", Session.FS, new HashSet<>(List.of(a48, a67)));
+        Course b63 = new Course("CSCB63", Session.WS, new HashSet<>(List.of(b36)));
 
-        Cours c24 = new Cours("CSCC24", Session.WS, new HashSet<>(List.of(b07, b09)));
-        Cours c63 = new Cours("CSCC63", Session.FW, new HashSet<>(List.of(b63, b36)));
+        Course c24 = new Course("CSCC24", Session.WS, new HashSet<>(List.of(b07, b09)));
+        Course c63 = new Course("CSCC63", Session.FW, new HashSet<>(List.of(b63, b36)));
 
         System.out.println("Test taken nothing");
-        Set<Cours> taken = new HashSet<>();
-        Set<Cours> want = new HashSet<>(List.of(c24, a08, a48, b07, b36, a67));
-        Map<String, Set<Cours>> output = generate(taken, want, new Semester(2022, Session.FALL));
+        Set<Course> taken = new HashSet<>();
+        Set<Course> want = new HashSet<>(List.of(c24, a08, a48, b07, b36, a67));
+        Map<String, Set<Course>> output = generate(taken, want, new Semester(2022, Session.FALL));
         System.out.println("\tTaken: " + taken);
         System.out.println("\tTimeline: " + output);
 
