6183038b7753163689121dba30457496906c721a
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-11-20 20:02:48 -0500

    feat(User) login

--- app/src/main/java/com/example/b07/User.java
+++ app/src/main/java/com/example/b07/User.java
@@ -1,73 +1,95 @@
 package com.example.b07;
 
-import android.content.Intent;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import com.example.b07.databinding.FragmentSecondBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.spec.ECField;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
-public final class User extends AppCompatActivity {
-    // todo: should be singleton
-    private static User user;
-    private DatabaseReference mDatabase;
-
-    public String username;
-    private String password;
-    public String[] courses;
+public final class User {
+    private static final User instance = new User();
+    public String name;
     public boolean privileged;
+    public boolean exists;
+    public boolean isin;
 
-    private User(String username, String password, String[] courses, boolean privileged) {
-        this.username = username;
-        this.password = password;
-        this.courses = courses;
-        this.privileged = privileged;
+    private User() {
     }
 
-    public boolean isUserExists(String username) {
-        DatabaseReference UserReference = FirebaseDatabase.getInstance().getReference().child("users");
-        return false;
-    }
-
-
-    public static User login(String username, String password) throws NoSuchAlgorithmException {
-        DatabaseReference UserReference = FirebaseDatabase.getInstance().getReference().child("users");
-        boolean isUserExist = user.isUserExists(username);
-
-        if (isUserExist) {
-            String desiredPassword = UserReference.child(username + password).toString();
-
+    /**
+     * Encrypt to sha256
+     *
+     * @param message
+     * @return
+     */
+    public static String sha256(String message) {
+        // My IDE: Unhandled exception: NoSuchAlgorithmException
+        // Me: trust me bro
+        try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-
-            if (desiredPassword.equals(hash.toString())) {
-
-//                user = new User(username, password, );
-            }
+            byte[] bytes = digest.digest(message.getBytes(StandardCharsets.UTF_8));
+            return String.format("%064x", new BigInteger(1, bytes));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            System.err.println("yo wdym you dont know what sha256 is");
         }
+        return "";
+    }
 
-        return null;
+    public static User getInstance() {
+        return instance;
+    }
+
+    public static User login(String name, String password) {
+        User.instance.name = name;
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child(name);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object user = snapshot.getValue();
+                // check existence
+                if (user == null) {
+                    User.instance.exists = false;
+                    return;
+                }
+                User.instance.exists = true;
+
+                // check password
+                String hash = snapshot.child("passwd").getValue(String.class);
+                if (!Objects.equals(sha256(password), hash)) {
+                    User.instance.isin = false;
+                    return;
+                }
+                User.instance.isin = true;
+                User.instance.privileged = Boolean.TRUE.equals(snapshot.child("privileged").getValue(Boolean.class));
+                Log.i("User", name + " logged in");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("User", "login cancelled");
+            }
+        });
+        return instance;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "User{name='" + name + "'" + ", privileged=" + privileged + '}';
     }
 
     // similar to getManager
@@ -77,18 +99,29 @@ public final class User extends AppCompatActivity {
         2. add new user to database (privileged is always false)
         3. return the created user
          */
-        DatabaseReference UserReference = FirebaseDatabase.getInstance().getReference().child("users");
-        boolean isUserExist = user.isUserExists(username);
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-        if (isUserExist) {
-            return null;
-        }
+            }
 
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+//        boolean isUserExist = user.isUserExists(username);
+//
+//        if (isUserExist) {
+//            return null;
+//        }
+//
+//        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+//        byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
 
 //        UserReference.
 
-        return user;
+        return null;
     }
 }
