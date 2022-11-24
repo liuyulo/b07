c5545dbf54f178616e3c11b49734bf14be5dff93
In Kim <in.kim0128@gmail.com>
2022-11-24 02:51:04 -0500

    Update FirstFragment.java

--- app/src/main/java/com/example/b07/FirstFragment.java
+++ app/src/main/java/com/example/b07/FirstFragment.java
@@ -1,14 +1,21 @@
 package com.example.b07;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.b07.databinding.FragmentFirstBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FirstFragment extends Fragment {
 
@@ -22,7 +29,49 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        User u = User.login("admin", "password");
+        TextView tv = (TextView) view.findViewById(R.id.textview_first);
+        DatabaseReference user = FirebaseDatabase.getInstance().getReference("users");
+        user.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (u == null) {
+                    tv.setText("User not found");
+                } else {
+                    tv.setText(u.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Add log when refactoring for warning
+            }
+        });
+
+
+        //TextView.setText
+
+        /*
+        
+        binding.buttonFirst.setOnClickListener(
+            view1 -> {
+                // set value
+                Date date = new Date();
+//                Calendar c = Calendar.getInstance();
+//                c.setTime(date);
+//                now.setValue(c.get(Calendar.MONTH));
+                Log.i("firebase", String.valueOf(date.getTime()));
+                now.setValue(date.getTime());
+                NavHostFragment.findNavController(FirstFragment.this).navigate(
+                    R.id.action_FirstFragment_to_SecondFragment
+                );
+            }
+        );
+         */
+
         view.findViewById(R.id.test).setOnClickListener(v -> {
+
+
             // test your features here
             // and click the TEST button to trigger
         });
