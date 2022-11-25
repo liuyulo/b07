df4a77177ad5e5c030e1c2410627b60cb4548f9d
Jiaming Lu <jiaming.lu.0124@gamil.com>
2022-11-24 23:38:28 -0500

    feat: remove method of Student class

--- app/src/main/java/com/example/b07/Student.java
+++ app/src/main/java/com/example/b07/Student.java
@@ -55,15 +55,13 @@ public class Student extends User{
     }
 
     /**
-     * add course c into the current database
+     * add course c to "users/student/name/course"
+     * Precondition: c is in 'courses'
      * @param c
      * @return true if and only course does not exists and succesfully added
      */
     @Override
     public boolean add(Course c){
-        // add course to `users[name].courses`
-        // assume c is in `courses'
-
         //init isin to check if course already exists
         Student.instance.isin = false;
         DatabaseReference cRef = ref.child(name).child("course");
@@ -71,26 +69,31 @@ public class Student extends User{
         cRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.d(TAG, "add: adding the course into the database");
+                Log.d(TAG, "add: trying to add to the course into the database");
 
                 // count the number of courses in the course node and insert the new course at the end
                 int cnt = 0;
                 for (DataSnapshot ds: snapshot.getChildren()) {
                     if (ds.child(String.valueOf(cnt)).getChildren().toString().equals(c.code)) {
+                        Log.d(TAG, "add: course already exists, updating unsuccessful");
                         Student.instance.isin = true;
                     }
                     cnt++;
                 }
                 if (!Student.instance.isin) {
+                    Log.d(TAG, "add: updating successful");
                     cRef.child(String.valueOf(cnt)).setValue(c.code);
+                    return;
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.d(TAG, "add: failed to add course");
+                Log.d(TAG, "add: error(s) happened, failed to add the course");
             }
         });
+
+        Log.d(TAG, "add: quitting the method");
         if (Student.instance.isin) {
             return true;
         }
@@ -99,12 +102,61 @@ public class Student extends User{
         }
     }
 
+    /**
+     * remove course from "users/student/course"
+     * @param c
+     * @return true if and only if course c is in the list of courses and is successfully removed
+     */
     @Override
     public boolean remove(Course c){
         // remove course from `users[name].courses`
+        Student.instance.isin = false;
+        ArrayList<String> courses = new ArrayList<String>();
+        final DatabaseReference cRef = ref.child(name).child("course");
+
+        cRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d(TAG, "remove: trying to remove from the database");
+                int cnt = 0;
+                for (DataSnapshot ds: snapshot.getChildren()) {
+                    String code = ds.getValue().toString();
+                    // if found the course then skip it, dont add it into the list of courses
+                    if (code.equals(c.code)) {
+                        Log.d(TAG,"remove: remove successful");
+                        Student.instance.isin = true;
+                        continue;
+                    }
+                    courses.add(code);
+
+                    // delete the view course to enable the rearrangement of indices of courses later
+                    cRef.child(String.valueOf(cnt)).removeValue();
+                }
+                if (!Student.instance.isin) {
+                    Log.d(TAG, "remove: remove unsuccessful, course does not exists");
+                }
+
+                // update the course list
+                Log.d(TAG, "remove: updating the course list");
+                for (int i = 0; i < courses.size(); i++) {
+                    cRef.child(String.valueOf(i)).setValue(courses.get(i));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d(TAG, "remove: Errors happened");
+            }
+        });
 
 
-        return false;
+        Log.d(TAG, "remove: quitting the method");
+        if (Student.instance.isin) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
 
