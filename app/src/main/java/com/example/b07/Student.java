af6b301b4e1076f4a9e7816d512acc27b83439d5
Jiaming Lu <jiaming.lu.0124@gamil.com>
2022-11-24 22:50:11 -0500

    added documentation for add method

--- app/src/main/java/com/example/b07/Student.java
+++ app/src/main/java/com/example/b07/Student.java
@@ -37,6 +37,12 @@ public class Student extends User{
         return instance;
     }
 
+    /**
+     *
+     * @param name
+     * @param password
+     * @return Student typed instance
+     */
     public static Student signup(String name, String password){
         instance = new Student(name);
         instance.exists = instance.isin = true;
@@ -48,10 +54,18 @@ public class Student extends User{
         return instance;
     }
 
+    /**
+     * add course c into the current database
+     * @param c
+     * @return true if and only course does not exists and succesfully added
+     */
     @Override
     public boolean add(Course c){
         // add course to `users[name].courses`
         // assume c is in `courses'
+
+        //init isin to check if course already exists
+        Student.instance.isin = false;
         DatabaseReference cRef = ref.child(name).child("course");
         ArrayList<String> courses= new ArrayList<String>();
         cRef.addValueEventListener(new ValueEventListener() {
@@ -62,9 +76,14 @@ public class Student extends User{
                 // count the number of courses in the course node and insert the new course at the end
                 int cnt = 0;
                 for (DataSnapshot ds: snapshot.getChildren()) {
+                    if (ds.child(String.valueOf(cnt)).getChildren().toString().equals(c.code)) {
+                        Student.instance.isin = true;
+                    }
                     cnt++;
                 }
-                cRef.child(String.valueOf(cnt)).setValue(c.code);
+                if (!Student.instance.isin) {
+                    cRef.child(String.valueOf(cnt)).setValue(c.code);
+                }
             }
 
             @Override
@@ -72,13 +91,18 @@ public class Student extends User{
                 Log.d(TAG, "add: failed to add course");
             }
         });
-
-        return true;
+        if (Student.instance.isin) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
     public boolean remove(Course c){
         // remove course from `users[name].courses`
+
 
         return false;
     }
