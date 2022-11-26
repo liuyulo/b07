e4958722f9d14906f58ff8b36b7bf0ff9e3ce0f8
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-11-26 17:05:27 -0500

    feat: show timeline

--- app/src/main/java/com/example/b07/FirstFragment.java
+++ app/src/main/java/com/example/b07/FirstFragment.java
@@ -18,9 +18,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private final String TAG = "FirstFragment";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,21 +35,29 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        User u = User.login("admin", "password");
-        TextView tv = (TextView) view.findViewById(R.id.textview_first);
-        if (u == null) {
-            tv.setText("User not found");
-        } else {
-            tv.setText(u.name);
-        }
-
-        binding.buttonFirst.setOnClickListener(
-                view1 -> {
-                    NavHostFragment.findNavController(FirstFragment.this).navigate(
-                            R.id.action_FirstFragment_to_SecondFragment
-                    );
-                }
+//        Account.login("student", "student");
+        Account.login("admin", "password");
+        User u = Admin.getInstance();
+        TextView t = view.findViewById(R.id.textview_first);
+        // see values/strings.xml
+        t.setText(getResources().getString(R.string.first_fragment_user, u.name));
+        view.findViewById(R.id.button_timeline).setOnClickListener(
+            v -> NavHostFragment.findNavController(FirstFragment.this).navigate(
+                R.id.action_First_to_Timeline
+            )
         );
+//        Course c = Course.from("CSCB07");
+//        Course c = new Course("aaaa01", Session.FS, Set.of(Course.from("CSCB07")));
+//        AtomicInteger count = new AtomicInteger();
+//        view.findViewById(R.id.test).setOnClickListener(v -> {
+//            if (count.get() % 2 == 0) {
+//                Log.d(TAG, "add " + u.add(c));
+//            } else {
+//                Log.d(TAG, "remove " + u.remove(c));
+//            }
+//            Log.d(TAG, String.valueOf(u.courses.stream().map(o -> o.code).collect(Collectors.toSet())));
+//            count.getAndIncrement();
+//        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/b07/TimelineFragment.java
@@ -0,0 +1,60 @@
+package com.example.b07;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class TimelineFragment extends Fragment {
+
+    private static final String ARG_COLUMN_COUNT = "column-count";
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TimelineFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static TimelineFragment newInstance(int columnCount) {
+        TimelineFragment fragment = new TimelineFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_timeline_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView recyclerView) {
+            Context context = view.getContext();
+            Set<Course> want = Set.of(Course.from("cscc24"));
+            Semester cur = new Semester(2022, Session.FALL);
+            Map<String, Set<Course>> todo = Timeline.generate(Set.of(), want, cur);
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            recyclerView.setAdapter(new TimelineViewAdapter(todo));
+        }
+        return view;
+    }
+}
