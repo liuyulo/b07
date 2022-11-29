7be07604a7d15257bdbad1e8dd6d581b81ba2928
In Kim <in.kim0128@gmail.com>
2022-11-29 15:30:24 -0500

    Created WantFragment

---
+++ app/src/main/java/com/example/b07/WantFragment.java
@@ -0,0 +1,43 @@
+package com.example.b07;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WantFragment extends Fragment {
+
+    Student s = Student.getInstance();
+
+    public WantFragment() {
+        s.adapter = new CourseAdapter(() -> s.courses);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_want, container, false);
+        RecyclerView want = view.findViewById(R.id.want);
+        want.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        want.setAdapter(s.adapter);
+        return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity) getActivity()).findViewById(R.id.test_want).setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity) getActivity()).findViewById(R.id.test_want).setVisibility(View.VISIBLE);
+    }
+}
