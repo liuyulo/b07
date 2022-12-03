ca19e948d21a0f654d88aed79cca80635bcd3b82
Jiaming Lu <jiaming.lu.0124@gamil.com>
2022-12-02 22:12:25 -0500

    feat(TakenFragment) insert and delete btn

--- app/src/main/java/com/example/b07/TakenFragment.java
+++ app/src/main/java/com/example/b07/TakenFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -13,6 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 public class TakenFragment extends Fragment {
 
     Student s = Student.getInstance();
+    private Button btnInsert;
+    private Button btnDelete;
+    private EditText editTextInsert;
+    private EditText editTextDelete;
+
 
     public TakenFragment() {
         s.adapter = new CourseAdapter(() -> s.courses);
@@ -38,6 +45,28 @@ public class TakenFragment extends Fragment {
         RecyclerView taken = view.findViewById(R.id.taken);
         taken.setLayoutManager(new LinearLayoutManager(view.getContext()));
         taken.setAdapter(s.adapter);
+
+        btnInsert = view.findViewById(R.id.btn_insert);
+        btnDelete = view.findViewById(R.id.btn_delete);
+        editTextInsert = view.findViewById(R.id.edittext_insert);
+        editTextDelete = view.findViewById(R.id.edittext_delete);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputCourse = editTextInsert.getText().toString();
+                s.add(Course.from(inputCourse));
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputCourse = editTextDelete.getText().toString();
+                s.remove(Course.from(inputCourse));
+            }
+        });
+
         return view;
     }
 }
