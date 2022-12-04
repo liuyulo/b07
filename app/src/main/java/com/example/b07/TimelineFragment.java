0bac6e2cb93b5b59d5ed16bbcd0560cbbae5e278
Yulong Liu <yulo.liu@mail.utoronto.ca>
2022-12-04 13:38:12 -0500

    refactor(Main,Timeline) optimise imports

--- app/src/main/java/com/example/b07/TimelineFragment.java
+++ app/src/main/java/com/example/b07/TimelineFragment.java
@@ -6,11 +6,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
